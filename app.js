b1985aaae3823792aca3645a674c4191d7af69f3
seacat271 <105187184+seacat271@users.noreply.github.com>
2023-05-25 15:06:13 +0300

    add json mid

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const logger = require("morgan");
+const cors = require("cors");
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello Kirill and Sanya");
 });
 app.post("/cart", (req, res) => {
-  res.send("Hello Kirill and Sanya");
+  const { name, email, phone, address } = req.body;
 });
 
 app.listen(8000, () => {
